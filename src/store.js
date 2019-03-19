ec8d56221be2eb3c9f4e25680e14658393fd94cf
Felipe Sena <felipedepaulasena@hotmail.com>
2019-03-19 09:49:07 -0300

    Add environment variable for server URL

--- src/store.js
+++ src/store.js
@@ -26,8 +26,8 @@ export default new Vuex.Store({
     actions: {
         async initialLoad(context) {            
             if (localStorage.bgtrackerjwt) {                                
-                Vue.axios.defaults.headers.common.Authorization = `Bearer ${localStorage.bgtrackerjwt}`;                
-                const res = await Vue.axios.get("http://localhost:5000/api/auth/user");                          
+                Vue.axios.defaults.headers.common.Authorization = `Bearer ${localStorage.bgtrackerjwt}`;                           
+                const res = await Vue.axios.get(process.env.VUE_APP_SERVER_HOST + "/api/auth/user");                          
                 context.commit("CURRENT_USER_FETCHED", res.data);
             }            
         }
